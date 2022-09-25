b655baa8a6ee54248f02d21f6f330eb16bec5f94
Jonathan Colo <colojonathan18@gmail.com>
2022-09-25 11:59:57 -0500

    Se modifica serializadores medico y enfermero

--- medicli/serializers/enfermeroSerializer.py
+++ medicli/serializers/enfermeroSerializer.py
@@ -1,7 +1,10 @@
 from medicli.models.enfermero import Enfermero
 from rest_framework import serializers
+from medicli.serializers.userSerializer import UserSerializer
 
 class EnfermeroSerializer(serializers.ModelSerializer):
+
+    user=UserSerializer()
     class Meta:
         model = Enfermero
         fields = ['cedula', 'nombre', 'apellido', 'direccion', 'telefono', 'ciudad', 'fecha', 'paciente']
--- medicli/serializers/medicoSerializer.py
+++ medicli/serializers/medicoSerializer.py
@@ -1,7 +1,10 @@
 from medicli.models.medico import Medico
 from rest_framework import serializers
+from medicli.serializers.userSerializer import UserSerializer
 
 class MedicoSerializer(serializers.ModelSerializer):
+
+    user=UserSerializer()
     class Meta:
         model = Medico
         fields = ['cedula', 'especialidad', 'nombre', 'apellido', 'direccion', 'telefono', 'ciudad', 'fecha', 'paciente']
