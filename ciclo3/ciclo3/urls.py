ac0b6bee6d992c19c735c1c486005d813ae792ac
HectorRojas93 <112224217+HectorRojas93@users.noreply.github.com>
2022-10-01 15:43:45 -0500

    Add files via upload
    Actualizació Views

---
+++ ciclo3/ciclo3/urls.py
@@ -0,0 +1,39 @@
+"""ciclo3 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView)
+from medicli import views
+
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('user/', views.UserCreateView.as_view()),
+    path('user/<int:pk>/', views.UserDetailView.as_view()),
+    path('paciente/', views.PacienteList.as_view()),
+    path('paciente/<str:pk>/', views.PacienteDetail.as_view()),
+    path('medico/', views.MedicoList.as_view()),
+    path('medico/<str:pk>/', views.MedicoDetail.as_view()),
+    path('enfermero/', views.EnfermeroList.as_view()),
+    path('enfermero/<str:pk>/', views.EnfermeroDetail.as_view()),
+    path('familiar/', views.FamiliarList.as_view()),
+    path('familiar/<str:pk>/', views.FamiliarDetail.as_view()),
+    path('diagnostico/', views.DiagnosticoList.as_view()),
+    path('diagnostico/<int:pk>/', views.DiagnosticoDetail.as_view()),
+    path('signos_vitales/', views.Signos_vitalesList.as_view()),
+    path('signos_vitales/<str:pk>/', views.Signos_vitalesDetail.as_view()),
+]
