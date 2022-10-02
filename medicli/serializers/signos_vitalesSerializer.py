82cec83f4ab2e3f0a381a2aa7e2125982e012a6d
HectorRojas93 <112224217+HectorRojas93@users.noreply.github.com>
2022-10-01 21:57:05 -0500

    Add files via upload

--- medicli/serializers/signos_vitalesSerializer.py
+++ medicli/serializers/signos_vitalesSerializer.py
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
 from medicli.models.signos_vitales import Signos_vitales
 from rest_framework import serializers
-from medicli.serializers.userSerializer import UserSerializer
 
 class Signos_vitalesSerializer(serializers.ModelSerializer):
     
-    user=UserSerializer()
     class Meta:
         model = Signos_vitales
-        fields = ['id','fecha_toma','oximetria',
-                  'frecuencia_respiratoria','frecuencia_Cardiaca','temperatura',
-=======
-from medicli.models.signos_vitales import Signos_vitales
-from rest_framework import serializers
-from medicli.serializers.userSerializer import UserSerializer
+        fields = ['id','fecha_toma','oximetria','frecuencia_respiratoria','frecuencia_cardiaca','temperatura','presion_arterial','glicemi','historia']
 
-class Signos_vitalesSerializer(serializers.ModelSerializer):
-    
-    user=UserSerializer()
-    class Meta:
-        model = Signos_vitales
-        fields = ['id','fecha_toma','oximetria',
-                  'frecuencia_respiratoria','frecuencia_Cardiaca','temperatura',
->>>>>>> 9f3c624df0653642d6d86f5baf07842de7bcea56
-                  'presion_arterial','glicemia','historia']
